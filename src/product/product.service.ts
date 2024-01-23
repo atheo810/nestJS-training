f7043a2dabb1003cfa2452b0e037f8271886e70a
atheo810 <atheos810@gmail.com>
2024-01-23 19:47:43 +0800

    feat: product service

---
+++ src/product/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductService {
+  private readonly products: Product[] = [];
+
+  create(product: Product) {
+    this.products.push(product);
+  }
+
+  findAll(): Product[] {
+    return this.products;
+  }
+}
