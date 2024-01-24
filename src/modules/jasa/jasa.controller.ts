20cbf2aa43b8f7888b0521e891051a75df38bf6a
atheo810 <atheos810@gmail.com>
2024-01-24 18:48:09 +0800

    feat: jasa controller

--- src/modules/jasa/jasa.controller.ts
+++ src/modules/jasa/jasa.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { JasaService } from './jasa.service';
+import { jasa as JasaModel } from '@prisma/client';
 
-@Controller()
-export class JasaController {}
+@Controller('jasa')
+export class JasaController {
+  constructor(private readonly jasaService: JasaService) {}
+  //   function Get Data
+  @Get()
+  async list(): Promise<JasaModel[]> {
+    return this.jasaService.list();
+  }
+
+  // @Post()
+  // async create(@Body() body: any) {
+  //   const { name, description } = body;
+  //   return this.jasaService.create(name, description);
+  // }
+}
