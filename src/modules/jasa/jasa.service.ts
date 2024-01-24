e70d15fd185c7047ec0921a9985802040c277d6d
atheo810 <atheos810@gmail.com>
2024-01-24 18:47:48 +0800

    feat: jasa service

--- src/modules/jasa/jasa.service.ts
+++ src/modules/jasa/jasa.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { jasa as JasaModel } from '@prisma/client';
 
 @Injectable()
 export class JasaService {
   constructor(private prisma: PrismaService) {}
-
   //   membuat function async list untuk di panggil di controller
-  async list() {
-    return await this.prisma.jasa.findMany();
+  async list(): Promise<JasaModel[]> {
+    return await this.prisma.jasa.findMany({});
   }
 
   //   membuat function async create utnuk di panggil di controller
-  async create(name: string, description: string) {
-    return await this.prisma.jasa.create({
-      data: {
-        name,
-        description,
-      },
-    });
-  }
+  // async create(name: string, description: string) {
+  //   return await this.prisma.jasa.create({
+  //     data: {
+  //       name,
+  //       description,
+  //     },
+  //   });
+  // }
 }
