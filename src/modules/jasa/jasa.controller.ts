1e22d12169cb03911062745933f78cbe87a7d85d
atheo810 <atheos810@gmail.com>
2024-01-24 19:49:23 +0800

    feat: jasa getById

--- src/modules/jasa/jasa.controller.ts
+++ src/modules/jasa/jasa.controller.ts
@@ -1,19 +1,29 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { JasaService } from './jasa.service';
 import { jasa as JasaModel } from '@prisma/client';
 
 @Controller('jasa')
 export class JasaController {
   constructor(private readonly jasaService: JasaService) {}
-  //   function Get Data
+  //   function Get All Jasa Data
   @Get()
+  @HttpCode(200)
   async list(): Promise<JasaModel[]> {
     return this.jasaService.list();
   }
 
-  // @Post()
-  // async create(@Body() body: any) {
-  //   const { name, description } = body;
-  //   return this.jasaService.create(name, description);
-  // }
+  // get specific jasa data
+  @Get(':id')
+  @HttpCode(200)
+  async byId(@Param('id') id: string): Promise<JasaModel> {
+    return this.jasaService.getById(id);
+  }
+
+  // function post data
+  @Post()
+  @HttpCode(201)
+  async create(@Body() body: any): Promise<JasaModel> {
+    const { name, description } = body;
+    return this.jasaService.create(name, description);
+  }
 }
